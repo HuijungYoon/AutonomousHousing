8edc3284edee4db9568b93756dcb3cc99eccbafa
HuijungYoon <markup3604@gmail.com>
2020-03-12 01:48:56 +0900

    update: slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
+import "./style.css";
+import CarouselSection, { secionOneList } from "./sections/CarouselSection";
 
 function App() {
-  return <div className="App">hello react</div>;
+  const onLeave = (origin, destination, direction) => {
+    console.log("Leaving section " + origin.index);
+  };
+  const afterLoad = (origin, destination, direction) => {
+    console.log("After load: " + destination.index);
+  };
+
+  return (
+    <>
+      <ReactFullpage
+        scrollOverflow={true}
+        // navigation={true}
+        slidesNavigation={true}
+        slidesNavPosition="bottom"
+        // sectionsColor={["orange", "purple", "green"]}
+        onLeave={onLeave}
+        controlArrows={false}
+        afterLoad={afterLoad}
+        render={({ state, fullpageApi }) => {
+          const moveTo = () => {
+            // fullpageApi.moveTo(1, 0);
+            fullpageApi.moveSlideRight();
+          };
+
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section">{secionOneList}</div>
+              <div className="section">
+                <h3>Section 3</h3>
+
+                <button onClick={moveTo}>Move top</button>
+              </div>
+            </div>
+          );
+        }}
+        afterRender={({ fullpageApi }) => {
+          setInterval(() => {
+            console.log(fullpageApi);
+            // fullpageApi.moveTo(1, 0);
+          }, 3000);
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/sections/CarouselSection.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./CarouselSection.css";
+import cn from "classnames";
+const sectionOneData = [
+  {
+    id: 1,
+    className: "firstSlide",
+    backgroundImage: require("../images/carousel_image1.png"),
+    title: "한국자율주택이 함께합니다!",
+    subTitle: "오래된 주택을 보유하고 계신다면"
+  },
+  {
+    id: 2,
+    className: "secondSlide",
+    backgroundImage: require("../images/carousel_image2.png"),
+    title: "오래되고 낡은 집, 고민이시죠?",
+    subTitle: "오래된 주택을 보유하고 계신다면"
+  },
+  {
+    id: 3,
+    className: "thridSlide",
+    backgroundImage: require("../images/carousel_image3.png"),
+    title: "자율주택정비사업을 신청하세요",
+    subTitle: "오래된 주택을 보유하고 계신다면"
+  },
+
+  {
+    id: 4,
+    className: "fourthSlide",
+    backgroundImage: require("../images/carousel_image4.png"),
+    title: "정부가 사업비를 지원합니다",
+    subTitle: "오래된 주택을 보유하고 계신다면"
+  }
+];
+
+export const secionOneList = sectionOneData.map(x => {
+  console.log(x.className);
+  return (
+    <>
+      <div className={cn("slide", x.className)}>
+        <img src={require("../images/logo.png")} className="logo" alt="logo" />
+        <span className={cn("slid", "title")}>{x.title}</span>
+        <span className={cn("slid", "subtitle")}>{x.subTitle}</span>
+        <img src={require("../images/mousescrolldown.png")} alt="logo" />
+      </div>
+    </>
+  );
+});
+
+const CarouselSection = () => {
+  return <>{secionOneList}</>;
+};
+
+export default CarouselSection;
